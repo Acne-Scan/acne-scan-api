30d1d4345063a2cfc0e3458299cf950868bc0b4f
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-09 20:52:06 +0800

    fix: change error handling

--- internal/pkg/jwt/extract_token.go
+++ internal/pkg/jwt/extract_token.go
@@ -3,7 +3,6 @@ package jwt
 import (
 	"acne-scan-api/internal/model/web"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -14,13 +13,17 @@ func ExtractToken(tokenString string) (*web.AuthResponse, error) {
 	type MyCustomClaims struct {
 		Username string `json:"username"`
 		Role     string `json:"role"`
-		UserId   int `json:"user_id"`
+		UserId   int    `json:"user_id"`
 		jwt.RegisteredClaims
 	}
 
 	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("JWT_SECRET")), nil
 	})
+
+	if err != nil {
+		return nil, fmt.Errorf("invalid token: %s",err.Error())
+	}
 
 	if claims, ok := token.Claims.(*MyCustomClaims); ok && token.Valid {
 		extractedToken := &web.AuthResponse{
@@ -29,10 +32,6 @@ func ExtractToken(tokenString string) (*web.AuthResponse, error) {
 			UserId:   claims.UserId,
 		}
 		return extractedToken, nil
-	}
-
-	if err != nil {
-		log.Fatal(err)
 	}
 
 	return nil, fmt.Errorf("invalid token")
