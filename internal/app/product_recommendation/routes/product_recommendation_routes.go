6ff03e4ec0e141a8dc0db38dd3f567083e55bba0
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-11 07:16:38 +0800

    chore: inactive middleware

--- internal/app/product_recommendation/routes/product_recommendation_routes.go
+++ internal/app/product_recommendation/routes/product_recommendation_routes.go
@@ -1,16 +1,14 @@
 package routes
 
 import (
-	"acne-scan-api/internal/middleware"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func (ar *ProductRecommendationRoutesImpl) ProductRecommendationRoutes(app *fiber.App) {
-	app.Post("/recommendations", middleware.AdminMiddleware(), ar.productRecommendationHandler.Create)
+	app.Post("/recommendations",  ar.productRecommendationHandler.Create)
 	app.Get("/recommendations", ar.productRecommendationHandler.GetAll)
 	app.Get("/recommendations/:id", ar.productRecommendationHandler.GetById)
-	app.Patch("/recommendations/:id", middleware.AdminMiddleware(), ar.productRecommendationHandler.Update)
-	app.Delete("/recommendations/:id", middleware.AdminMiddleware(), ar.productRecommendationHandler.Delete)
+	app.Patch("/recommendations/:id",  ar.productRecommendationHandler.Update)
+	app.Delete("/recommendations/:id",  ar.productRecommendationHandler.Delete)
 
 }
