49cd29a18f121f6aa6601b425028e0c7dbd202a9
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-11 14:02:16 +0800

    feat: add return value

--- internal/app/history/repository/history_repository_get_all.go
+++ internal/app/history/repository/history_repository_get_all.go
@@ -10,19 +10,22 @@ func (history *HistoryRepositoryImpl) GetAll(id int) ([]*domain.History, error) 
 
 	result := []*domain.History{}
 
-	rows, err := history.DB.Query("select history.history_id, history.user_id,users.username, history.image, history.prediction, history.recommendation, history.created_at, history.updated_at from history left join users on users.user_id=history.user_id where users.user_id=?", id)
+	rows, err := history.DB.Query("select history.history_id, history.user_id, history.user_picture ,users.username, history.image, history.product_link, history.prediction, history.recommendation, history.created_at, history.updated_at from history left join users on users.user_id=history.user_id where users.user_id=?", id)
 	if err != nil {
 		return nil, err
 	}
 
 	for rows.Next() {
 		var imageData []byte
+		var ProductLinkData []byte
 		each := domain.History{}
 		err = rows.Scan(
 			&each.HistoryId,
 			&each.User_id,
+			&each.UserPicture,
 			&each.Name,
 			&imageData,
+			&ProductLinkData,
 			&each.Prediction,
 			&each.Recommendation,
 			&each.CreatedAt,
@@ -34,6 +37,10 @@ func (history *HistoryRepositoryImpl) GetAll(id int) ([]*domain.History, error) 
 
 		if err := json.Unmarshal(imageData, &each.Image); err != nil {
 			return nil, fmt.Errorf("failed to parse image JSON: %v", err)
+		}
+
+		if err := json.Unmarshal(ProductLinkData, &each.ProductLink); err != nil {
+			return nil, fmt.Errorf("failed to parse product link JSON: %v", err)
 		}
 
 		result = append(result, &each)
--- internal/app/history/repository/history_repository_get_by_id.go
+++ internal/app/history/repository/history_repository_get_by_id.go
@@ -10,12 +10,15 @@ func (history *HistoryRepositoryImpl) GetById(id int) (*domain.History, error) {
 	result := domain.History{}
 
 	var imageData []byte
+	var ProductLinkData []byte
 
-	err := history.DB.QueryRow("select history.history_id, history.user_id,users.username, history.image, history.prediction, history.recommendation, history.created_at, history.updated_at from history left join users on users.user_id=history.user_id where history_id=?", id).Scan(
+	err := history.DB.QueryRow("select history.history_id, history.user_id, history.user_picture ,users.username, history.image, history.product_link, history.prediction, history.recommendation, history.created_at, history.updated_at from history left join users on users.user_id=history.user_id where history_id=?", id).Scan(
 		&result.HistoryId,
 		&result.User_id,
+		&result.UserPicture,
 		&result.Name,
 		&imageData,
+		&ProductLinkData,
 		&result.Prediction,
 		&result.Recommendation,
 		&result.CreatedAt,
@@ -28,6 +31,10 @@ func (history *HistoryRepositoryImpl) GetById(id int) (*domain.History, error) {
 
 	if err := json.Unmarshal(imageData, &result.Image); err != nil {
 		return nil, fmt.Errorf("failed to parse image JSON: %v", err)
+	}
+
+	if err := json.Unmarshal(ProductLinkData, &result.ProductLink); err != nil {
+		return nil, fmt.Errorf("failed to parse product link JSON: %v", err)
 	}
 
 	return &result, nil
