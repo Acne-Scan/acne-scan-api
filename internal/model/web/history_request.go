2ef6f85f716a2a98553c51b6f481092bd35ea917
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-11 14:03:08 +0800

    feat: add return value

--- internal/model/web/history_request.go
+++ internal/model/web/history_request.go
@@ -5,6 +5,7 @@ type ProductLink map[string]interface{}
 
 type HistoryRequest struct {
 	User_id        uint        `json:"user_id" form:"user_id"`
+	HistoryId      string      `json:"history_id" form:"history_id"`
 	Image          Images      `json:"image" form:"image"`
 	UserPicture    string      `json:"user_picture" form:"user_picture"`
 	ProductLink    ProductLink `json:"product_links" form:"product_links"`
