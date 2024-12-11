4cc0cbf748ca6446cbed41229d732ffa179d731d
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-11 13:56:58 +0800

    feat: add new migration for change column data type

---
+++ internal/infrastructure/database/migrations/20241211033018_alter_table_article_change_data_type.sql.down.sql
@@ -0,0 +1,7 @@
+ALTER Table article MODIFY COLUMN description VARCHAR(512);
+
+ALTER Table article MODIFY COLUMN article_id INT;
+
+ALTER Table pruduct_recommendation MODIFY COLUMN recommendation_id INT;
+
+ALTER Table history MODIFY COLUMN history_id INT;
---
+++ internal/infrastructure/database/migrations/20241211033018_alter_table_article_change_data_type.sql.up.sql
@@ -0,0 +1,7 @@
+ALTER Table article MODIFY COLUMN description text;
+
+ALTER Table article MODIFY COLUMN article_id VARCHAR(255);
+
+ALTER Table pruduct_recommendation MODIFY COLUMN recommendation_id VARCHAR(255);
+
+ALTER Table history MODIFY COLUMN history_id VARCHAR(255);
